863f2f8d829cc23447b520bd09f882b384956a48
Rafael Schniedermann <r.schniedermann@ruga-verl.de>
2023-02-13 17:21:09 +0100

    Projektdateien hinzufügen.

---
+++ Program.cs
@@ -0,0 +1,82 @@
+﻿// See https://aka.ms/new-console-template for more information
+using Kadmium_sACN;
+using Kadmium_sACN.MulticastAddressProvider;
+using Kadmium_sACN.Layers.Framing;
+using Kadmium_sACN.SacnSender;
+using System.Text.Json.Serialization;
+using System.Text.Json;
+using Microsoft.Extensions.Configuration;
+using DMX_StartupSetter;
+
+Console.WriteLine("Hello, World!");
+
+var builder = new ConfigurationBuilder()
+                .SetBasePath(Directory.GetCurrentDirectory())
+                .AddJsonFile("config.json", optional: false);
+
+IConfiguration config = builder.Build();
+
+var settings = config.Get<Settings>();
+
+
+var sacnSender = new SacnSender();
+byte[] cid = new byte[16]; 
+Random.Shared.NextBytes(cid);
+
+string sourceName = settings.SourceName;
+var factory = new SacnPacketFactory(cid, sourceName);
+
+
+
+
+
+//using System.Timers.Timer timer = new System.Timers.Timer(10000);
+//timer.Elapsed += async (sender, e) =>
+//{
+//    var packets = factory.CreateUniverseDiscoveryPackets(new UInt16[] { universe });
+//    foreach (var packet in packets)
+//    {
+//        await sacnSender.SendMulticast(packet);
+//    }
+//    Console.WriteLine("sending req");
+//};
+
+
+
+
+
+using System.Timers.Timer timer2 = new System.Timers.Timer(10000);
+timer2.Elapsed += async (sender, e) =>
+{
+    var settings = config.Get<Settings>();
+    var text = await File.ReadAllTextAsync(settings.ConfigFile);
+    var dic = JsonSerializer.Deserialize<Dictionary<string, int>>(text);
+
+
+    UInt16 universe = (UInt16) settings.Universe;
+    byte[] data = new byte[512];
+
+    for (int i = 0; i < data.Length; i++)
+    {
+        if(dic != null && dic.TryGetValue((i + 1).ToString(), out int dVal))
+            data[i] = (byte)dVal;
+    }
+
+
+    var packets = factory.CreateUniverseDiscoveryPackets(new UInt16[] { universe });
+
+    var packet = factory.CreateDataPacket(universe, data);
+    await sacnSender.SendUnicast(packet, System.Net.IPAddress.Parse(settings.Destination));
+
+    Console.WriteLine("sending packages");
+};
+
+
+
+
+//timer.Start();
+timer2.Start();
+
+
+
+Console.ReadLine();
---
+++ Settings.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DMX_StartupSetter
+{
+    public class Settings
+    {
+        public string ConfigFile { set; get; } = "data.json";
+        public string SourceName { set; get; } = "Basic Dimmer";
+        public string Destination { set; get; } = "127.0.0.1";
+        public int Universe { set; get; } = 1;
+    }
+}
